981073d1597ddae0535c32c5beca7c522a6615ee
chellel <1010603372@qq.com>
2025-09-18 13:39:05 +0800

    feat: 配置环境变量

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GeminiModule } from './gemini/gemini.module';
+import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
 
 @Module({
-  imports: [GeminiModule],
+  imports: [
+    ConfigModule.forRoot({
+      expandVariables: true,
+      // 全局可以使用
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      load: [() => dotenv.config()],
+    }),
+    GeminiModule],
   controllers: [AppController],
   providers: [AppService],
 })
